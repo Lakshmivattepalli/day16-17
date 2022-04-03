d133654da482963d4b31c14743d332d9803743ea
srikala <vlslaksh1899@gmail.com>
2022-04-03 19:30:32 +0530

    added

---
+++ binary/BinaryTree.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BinarySearchTree
+{
+    internal class BinaryTree
+    {
+        public object Root { get; internal set; }
+
+        internal void Add(int v)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void TraversePreorder(object root)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
